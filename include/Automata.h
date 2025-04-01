689c767be3e323cd431239c965cbe5f8012c6d42
ArtemFedorov2004 <artem20.fedorov00@mail.ru>
2025-04-01 15:32:28 +0300

    include Automata.h

---
+++ include/Automata.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <map>
+#include <ostream>
+#include <string>
+
+enum class STATE {
+	OFF,
+	WAIT,
+	ACCEPT,
+	CHECK,
+	COOK
+};
+
+class Automata {
+public:
+	Automata(std::ostream& output) : out(output), cash(0), state(STATE::OFF) {
+		menu = {
+			{"Coffe", 500},
+			{"Tea", 40},
+			{"Juice", 160}
+		};
+	}
+
+	void on();
+	void off();
+	void coin(int money);
+	void choice(std::string drink);
+	void cancel();
+	STATE getState();
+	void logMenu();
+	int getCash();
+
+private:
+	std::ostream& out;
+	int cash;
+	std::map<std::string, int> menu;
+	STATE state;
+	void check(std::string drink);
+	void cook();
+	void finish();
+};
